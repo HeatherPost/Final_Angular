2c2361c0b75d6390f0da8f1e18e4b53097b1432f
HeatherPost <h_npost@student.fdu.edu>
2017-12-19 18:49:34 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './components/teacher/teacher.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
