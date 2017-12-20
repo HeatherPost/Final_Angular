74bfb7775ff64a288b401f744968e9c8a53c7dbe
HeatherPost <h_npost@student.fdu.edu>
2017-12-19 20:39:54 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { teacherComponent } from './components/teacher/teacher.component';
+import {TeacherListDataService} from './services/teacher-data.service'
 //import { AboutComponent } from './components/about/about.component';
 
+const appRoutes: Routes = [
+  {path: '', component: teacherComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,9 +21,12 @@ import { teacherComponent } from './components/teacher/teacher.component';
     //AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [TeacherListDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
